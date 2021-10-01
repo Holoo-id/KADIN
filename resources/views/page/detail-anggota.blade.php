6b613bb0299b7d15663b21173c2882140e3461ab
Andika .R <andika15ramdani@gmail.com>
2021-10-01 18:41:06 +0700

    20211001 - 1840 - revisi data anggota

--- resources/views/page/detail-anggota.blade.php
+++ resources/views/page/detail-anggota.blade.php
@@ -1,36 +1,36 @@
 @extends('base')
 @section('content')
 <div class="row">
-		<div class="row col-md-4">
-			<div class="col-md-3 position-fixed">
-				<div class="card card-profile">
-					<div class="card-avatar">
-						<a href="javascript:;">
-							<img class="img" src="https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg">
-						</a>
-					</div>
-					<div class="card-body">
-						<h6 class="card-category text-gray">CEO / Co-Founder</h6>
-						<h4 class="card-title">Alec Thompson</h4>
-						{{-- <p class="card-description">
-							Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-						</p>
-						<a href="javascript:;" class="btn btn-primary btn-round">Follow</a> --}}
-						<ul class="list-group">
-							<a href="#detailTabInfo" class="list-group-item list-group-item-action">INFORMASI</a>
-							<a href="#detailTabSosial" class="list-group-item list-group-item-action">SOSIAL MEDIA</a>
-							<a href="" class="list-group-item list-group-item-action">GALERI</a>
-							<a href="" class="list-group-item list-group-item-action">MAPS</a>
-						</ul>
-					</div>
+	<div class="row col-md-4">
+		<div class="col-md-3 position-fixed">
+			<div class="card card-profile">
+				<div class="card-avatar">
+					<a href="javascript:;">
+						<img class="img" src="https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg">
+					</a>
+				</div>
+				<div class="card-body">
+					<h6 class="card-category text-gray">Merek usaha goes here</h6>
+					<h4 class="card-title border-bottom pb-4">{{ $member->Nama }}</h4>
+					{{-- <p class="card-description">
+						Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
+					</p>
+					<a href="javascript:;" class="btn btn-primary btn-round">Follow</a> --}}
+					<ul class="list-group">
+						<a onclick="openTabs('personalInfo')" class="list-group-item list-group-item-action" style="cursor: pointer">INFORMASI ANGGOTA</a>
+						<a onclick="openTabs('bussInfo')" class="list-group-item list-group-item-action" style="cursor: pointer">INFORMASI USAHA</a>
+						<a href="" class="list-group-item list-group-item-action">GALERI</a>
+						<a href="" class="list-group-item list-group-item-action">MAPS</a>
+					</ul>
 				</div>
 			</div>
 		</div>
+	</div>
     <div class="col-md-8">
-      <div class="card" id="detailTabInfo">
+      <div class="card d-block" id="personalInfo">
         <div class="card-header card-header-primary">
           <h4 class="card-title">INFORMASI</h4>
-          <p class="card-category">Informasi Detail {{ $member->Nama }}</p>
+          <p class="card-category">Informasi Anggota {{ $member->Nama }}</p>
         </div>
         <div class="card-body">
 					<table class="table table-hover">
@@ -64,7 +64,7 @@
 									Usia
 								</td>
 								<td>
-									{{ \Carbon\Carbon::now()->year - \Carbon\Carbon::parse($member->tgl_lahir)->format('Y') }}
+									{{ \Carbon\Carbon::now()->year - \Carbon\Carbon::parse($member->tgl_lahir)->format('Y') }} tahun
 								</td>
 							</tr>
 							<tr>
@@ -95,14 +95,18 @@
 									<br>{{$member->kategori->provinsi}}
 								</td>
 							</tr>
-							<tr>
-								<td class="text-primary fw-bold">
-									Email Usaha
-								</td>
-								<td>
-									{{ $member->alamat }}
-								</td>
-							</tr>
+						</tbody>
+					</table>
+        </div>
+      </div>
+      <div class="card d-none" id="bussInfo">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">INFORMASI</h4>
+          <p class="card-category">Informasi Usaha {{ $member->Nama }}</p>
+        </div>
+        <div class="card-body">
+					<table class="table table-hover">
+						<tbody>
 							<tr>
 								<td class="text-primary fw-bold">
 									Jenis Usaha
@@ -132,7 +136,7 @@
 									Karyawan Usaha
 								</td>
 								<td>
-									{{ $member->jumlah_karyawan }}
+									{{ $member->jumlah_karyawan }} orang
 								</td>
 							</tr>
 							<tr>
@@ -140,7 +144,7 @@
 									Omset Usaha
 								</td>
 								<td>
-									{{ $member->alamat }}
+									Rp{{ $member->alamat }}
 								</td>
 							</tr>
 							<tr>
@@ -151,18 +155,14 @@
 									{{ $member->alamat }}
 								</td>
 							</tr>
-						</tbody>
-					</table>
-        </div>
-      </div>
-      <div class="card" id="detailTabSosial">
-        <div class="card-header card-header-primary">
-          <h4 class="card-title">Edit Profile</h4>
-          <p class="card-category">Complete your profile</p>
-        </div>
-        <div class="card-body">
-					<table class="table table-hover">
-						<tbody>
+							<tr>
+								<td class="text-primary fw-bold">
+									Email Usaha
+								</td>
+								<td>
+									{{ $member->alamat }}
+								</td>
+							</tr>
 							<tr>
 								<td class="text-primary fw-bold">
 									Telepon
@@ -181,14 +181,6 @@
 							</tr>
 							<tr>
 								<td class="text-primary fw-bold">
-									Email Usaha
-								</td>
-								<td>
-									example@usaha.mail
-								</td>
-							</tr>
-							<tr>
-								<td class="text-primary fw-bold">
 									Sosial Media Lain
 								</td>
 								<td>
@@ -201,4 +193,21 @@
       </div>
     </div>
   </div>
+  <script>
+		function openTabs(tabName) {
+			var info = document.getElementById("personalInfo");
+			var social = document.getElementById("bussInfo");
+			if (tabName === "personalInfo") {
+				info.classList.add("d-block");
+				info.classList.remove("d-none");
+				social.classList.add("d-none");
+				social.classList.remove("d-block");
+			} else if (tabName === "bussInfo") {
+				info.classList.add("d-none");
+				info.classList.remove("d-block");
+				social.classList.add("d-block");
+				social.classList.remove("d-none");
+			}
+		}
+	</script>
 @endsection
